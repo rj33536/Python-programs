1d812e70ee1218712214a5db540294e14b527e58
rj33536 <31067636+rj33536@users.noreply.github.com>
2018-07-09 16:54:25 +0530

    Update gradient_descent.py
    a vectorized implementation of gradient descent for linear regression. works faster than previous one.

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,28 +1,20 @@
 import numpy as np
+from numpy import matmul,transpose
+def gradientdescent_vectorized(inp,out,alpha,iterations):
+    length=np.shape(inp)
+    theta=np.zeros(length[1])
+    for i in range(iterations):
+        predicted=matmul(inp,transpose(theta))
+        theta=theta-alpha*(matmul(transpose(predicted-out),inp)/length[1])
+    return theta
 
-def gradient_descent(inp,out):
-    length=len(inp)
-    theta0=0
-    theta1=1
-    learning_rate=0.00001
-    min_cost=200
-    cost=200
-    while cost>0.00001:
-        predicted=theta1*inp+theta0
-        cost=sum((predicted-out)**2)/length
-        der1=sum((predicted-out)*inp)/length
-        der0=sum(predicted-out)/length
-        theta0=theta0-der0*learning_rate
-        theta1=theta1-der1*learning_rate
-    return (theta0 , theta1)
-       
-inp=np.array([1,2,3,4,5,6,7,8])
-out=np.array([1,4,9,16,25,36,49,64])
-theta0,theta1=gradient_descent(inp,out)
-print(theta0,theta1)
-print(theta0+theta1*10)
+def predict(X,theta):
+    return sum(X*theta)
+inp=np.array([[1,2],[1,3],[1,5],[1,6]])
+out=np.array([500,550,600,650])
     
-
+theta=gradientdescent_vectorized(inp,out,0.001,100000)
+print(predict([1,8],theta))
 
 
 
